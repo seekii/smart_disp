7df391c0ada63e8d763f36452a531dd80daa10fc
temmka <temmka88@gmail.com>
2018-01-14 01:13:22 +0200

    Test GPIO interrupt

--- main/main.c
+++ main/main.c
@@ -15,7 +15,48 @@
 
 
 
+#include <freertos/queue.h>
+#include <freertos/task.h>
 
+#include "sdkconfig.h"
+#include <esp_log.h>
+
+static char tag[] = "test_intr";
+static QueueHandle_t q1;
+
+#define TEST_GPIO (13)
+
+static void handler(void *args) {
+    gpio_num_t gpio;
+    gpio = TEST_GPIO;
+    xQueueSendToBackFromISR(q1, &gpio, NULL);
+}
+
+void test1_task(void *ignore) {
+    ESP_LOGD(tag, ">> test1_task");
+    gpio_num_t gpio;
+    q1 = xQueueCreate(10, sizeof(gpio_num_t));
+
+    gpio_config_t gpioConfig;
+    gpioConfig.pin_bit_mask = GPIO_SEL_13;
+    gpioConfig.mode         = GPIO_MODE_INPUT;
+    gpioConfig.pull_up_en   = GPIO_PULLUP_DISABLE;
+    gpioConfig.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    gpioConfig.intr_type    = GPIO_INTR_POSEDGE;
+    gpio_config(&gpioConfig);
+
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(TEST_GPIO, handler, NULL   );
+
+
+    while(1) {
+        ESP_LOGD(tag, "Waiting on interrupt queue");
+        BaseType_t rc = xQueueReceive(q1, &gpio, portMAX_DELAY);
+        ESP_LOGD(tag, "Woke from interrupt queue wait: %d", rc);
+    }
+
+    vTaskDelete(NULL);
+}
 
 
 void ws2812_task(void *pvParameters)
@@ -127,7 +168,8 @@ void main_display_task(void *pvParameter)
 
         writex(x);
 
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+         vTaskDelay(300 / portTICK_PERIOD_MS);
+
 
     }
 }
@@ -164,7 +206,7 @@ void app_main(void)
 
     xTaskCreate(main_display_task, "main_display_task", 1024, NULL, 5, NULL);
     xTaskCreate(ws2812_task, "ws2812 rainbow demo", 4096, NULL, 6, NULL);
-
+    xTaskCreate(test1_task, "test1_task", 4096, NULL, 6, NULL);
 
 }
 
