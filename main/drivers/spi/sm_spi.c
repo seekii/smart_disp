7c7a865bb4a3112fb9499fb5b6663aecacb7ad6b
temmka <temmka88@gmail.com>
2018-01-17 01:38:15 +0200

    Replace SPI transmit transaction method from direct to quened.

--- main/drivers/spi/sm_spi.c
+++ main/drivers/spi/sm_spi.c
@@ -88,19 +88,20 @@ void spi_write(spi_device_handle_t spi, uint8_t* data, uint16_t len)
 /*Write spi data with user field*/
 void spi_write_u(spi_device_handle_t spi, uint8_t* data, int len, void *user)
 {
-	esp_err_t ret;
-	spi_transaction_t t;
+    esp_err_t ret;
+    spi_transaction_t t;
 
-	if (len == 0)
-		return;             //no need to send anything
+    if(len == 0)
+    {
+        return;                                              //no need to send anything
+    }
 
-	memset(&t, 0, sizeof(t));       //Zero out the transaction
-	t.length = len * 8;        //Len is in bytes, transaction length is in bits.
-	t.tx_buffer = data;               //Data
-	t.user = user;                //D/C needs to be set to 1
-	ret = spi_device_transmit(spi, &t);  //Transmit!
-	assert(ret==ESP_OK);            //Should have had no issues.
-
+    memset(&t, 0, sizeof(t));                                //Zero out the transaction
+    t.length = len * 8;                                      //Len is in bytes, transaction length is in bits.
+    t.tx_buffer = data;                                      //Data
+    t.user = user;                                           //D/C needs to be set to 1
+    ret = spi_device_queue_trans(spi, &t, portMAX_DELAY);    //Transmit!
+    assert(ret==ESP_OK);                                     //Should have had no issues.
 }
 
 
